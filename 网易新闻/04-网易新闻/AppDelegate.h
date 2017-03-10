5f16d2a117dbe7f7d5a8fb5d3edc0a7271d9676c
王大牛 <daniel@DanielWangPro.local>
2017-03-10 19:48:51 +0800

    项目准备

---
+++ 网易新闻/04-网易新闻/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  04-网易新闻
+//
+//  Created by apple on 17/3/10.
+//  Copyright © 2017年 itheima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
